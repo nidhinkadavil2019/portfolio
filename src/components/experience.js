0edbf15bd802c528764341147ff9627382ecb1a7
Seema VK <seema_vk@homedepot.com>
2019-11-06 06:15:29 -0500

    git s

---
+++ src/components/experience.js
@@ -0,0 +1,128 @@
+import React, {Component} from 'react'
+
+class Experience extends Component{
+  render(){
+    return(
+      <div>
+        <section className="colorlib-experience" data-section="experience">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">Experience</span>
+                <h2 className="colorlib-heading animate-box">Work Experience</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                  <div className="timeline-centered">
+                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                        <div className="timeline-entry-inner">
+                          <div className="timeline-icon color-1">
+                              <i className="icon-pen2"></i>
+                          </div>
+                          <div className="timeline-label">
+                            <h2><a href="#">Sr. Data Architect/BI Lead at QSSI</a> <span>2011 - Present</span></h2>
+                            {/* <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p> */}
+                            <p>Managed One PI system software development team, performed planning, provided direction to developers, established project guidelines and standards to ensure high quality product.</p>
+                          </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry animate-box" data-animate-effect="fadeInRight">
+                        <div className="timeline-entry-inner">
+                          <div className="timeline-icon color-2">
+                              <i className="icon-pen2"></i>
+                          </div>
+                          <div className="timeline-label">
+                            <h2><a href="#">Technical Manager/Data Warehousing Architect - US Federal Aviation Administration </a> <span>2008-2011</span></h2>
+                              {/* <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p> */}
+                              <p>Managed all aspects of the project, from beginning to end. Created and re-engineered data warehouse environment, designed and implemented Business Intelligence solutions for the Federal Aviation Administration reporting needs.</p>
+                          </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                        <div className="timeline-entry-inner">
+                          <div className="timeline-icon color-3">
+                              <i className="icon-pen2"></i>
+                          </div>
+                          <div className="timeline-label">
+                            <h2><a href="#">Sr. Data Warehousing Manager - US Dept. of Veteran Affairs</a> <span>Apr 2008 - Nov 2008</span></h2>
+                            <p>Developed data extraction procedures to select data from VA modern and legacy source systems (e.g., BDN and Corporate database). Data extractions procedures were written using Informatica mappings and workflows, Oracle Snapshots, Oracle PL/SQL stored procedures, and UNIX shell scripts.</p>
+                          </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                        <div className="timeline-entry-inner">
+                          <div className="timeline-icon color-4">
+                              <i className="icon-pen2"></i>
+                          </div>
+                          <div className="timeline-label">
+                            <h2><a href="#">BI/ETL Architect - Centers for Medicare & Medicaid Services (CMS)</a> <span>2005 - 2008</span></h2>
+                            {/* <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p> */}
+                            <p>Led a group of six to eight developers in architecting and building the EDW for the reporting and analytics solutions. Conducted daily status and weekly reporting meetings of the group and update the senior management on the progress on timely basis</p>
+                          </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                        <div className="timeline-entry-inner">
+                          <div className="timeline-icon color-5">
+                            <i className="icon-pen2"></i>
+                          </div>
+                          <div className="timeline-label">
+                            <h2><a href="#">Sr. Data Warehouse Consultant at Meijer</a> <span>2004 - 2005</span></h2>
+                            {/* <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p> */}
+                            <p> Created/Modified MicroStrategy Project Sources and Projects using MicroStrategy Architect, Microstrategy Objects and Reports using MicroStrategy Desktop in 2-tier/ 3-tier/ 4-tier environment. Design custom Hierarchies, Security Filters, Roles and Drill Maps in MicroStrategy 7i that meet the business specifications.</p>
+                          </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                        <div className="timeline-entry-inner">
+                          <div className="timeline-icon color-1">
+                            <i className="icon-pen2"></i>
+                          </div>
+                          <div className="timeline-label">
+                            <h2><a href="#">Sr. Business Intelligence Analyst at Blue Cross and Blue Shield of Illinois</a> <span>2003 - 2004</span></h2>
+                            {/* <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p> */}
+                            <p>Interacted with business users for gathering the business requirements and also designed user friendly templates for the business partners to communicate any further enhancements needed.</p>
+                          </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                        <div className="timeline-entry-inner">
+                          <div className="timeline-icon color-2">
+                            <i className="icon-pen2"></i>
+                          </div>
+                          <div className="timeline-label">
+                            <h2><a href="#">Business Analyst at Noria Corporation</a> <span>Apr 2003 -  Dec 2003</span></h2>
+                            {/* <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p> */}
+                            <p>This was a very short span of time, played business analyst role as well as knowledge engineer</p>
+                          </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                        <div className="timeline-entry-inner">
+                          <div className="timeline-icon color-3">
+                            <i className="icon-pen2"></i>
+                          </div>
+                          <div className="timeline-label">
+                            <h2><a href="#">Research Assistant</a> <span>2001-2003</span></h2>
+                            {/* <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p> */}
+                            <p>Worked as research assistant in the area of data mining, neural network, and expert system at Oklahoma State University</p>
+                          </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                        <div className="timeline-entry-inner">
+                          <div className="timeline-icon color-none">
+                          </div>
+                        </div>
+                    </article>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+    );
+  }
+}
+export default Experience;
